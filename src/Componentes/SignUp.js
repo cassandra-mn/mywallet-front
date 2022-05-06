eca1c1dfb24f091062893db916935f7e3365edf6
cassandra-mn <arthenciac@gmail.com>
2022-05-05 23:27:20 -0300

    send input data

--- src/Componentes/SignUp.js
+++ src/Componentes/SignUp.js
@@ -1,14 +1,36 @@
 import styled from 'styled-components';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function SignUp() {
+    const [dados, setDados] = useState({
+        name: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    });
+
+    async function register() {
+        try {
+            await axios.post('http://localhost:5000/sign-up', dados);
+            alert('Cadastro realizado com sucesso!');
+        } catch(e) {
+            alert('Erro ao cadastrar');
+            alert(e.response.data);
+        }
+    }
+
     return (
         <Container>
             <Logo>MyWallet</Logo>
-            <Input placeholder='Nome'/>
-            <Input placeholder='E-mail'/>
-            <Input placeholder='Senha'/>
-            <Input placeholder='Confirme a senha'/>
-            <Button>Cadastrar</Button>
+
+            <Input placeholder='Nome' value={dados.name} onChange={e => setDados({...dados, name: e.target.value})}/>
+            <Input placeholder='E-mail' value={dados.email} onChange={e => setDados({...dados, email: e.target.value})}/>
+            <Input placeholder='Senha' type='password' value={dados.password} onChange={e => setDados({...dados, password: e.target.value})}/>
+            <Input placeholder='Confirme a senha' type='password' value={dados.passwordConfirm} onChange={e => setDados({...dados, passwordConfirm: e.target.value})}/>
+            
+            <Button onClick={register}>Cadastrar</Button>
+            
             <Login>Já tem uma conta? Entre agora?</Login>
         </Container>
     );
@@ -35,11 +57,13 @@ const Input = styled.input`
     width: 326px;
     height: 58px;
     padding: 15px;
+    font-size: 20px;
     margin-bottom: 13px;
     border-radius: 5px;
     border: none;
     background: #FFFFFF;
     box-sizing: border-box;
+    font-family: 'Raleway';
 
     ::placeholder {
         font-size: 20px;
