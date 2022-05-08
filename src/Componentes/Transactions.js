9c0b6b8c63272e691f24ee843c0a6303ff1fcb9e
cassandra-mn <arthenciac@gmail.com>
2022-05-08 16:44:41 -0300

    print transactions

--- src/Componentes/Transactions.js
+++ src/Componentes/Transactions.js
@@ -1,12 +1,14 @@
 import {useNavigate} from 'react-router-dom';
-import {FaSignOutAlt} from 'react-icons/fa';
 import {useContext, useEffect} from 'react';
+import {FaSignOutAlt} from 'react-icons/fa';
+import {useState} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
 import UserContext from './../context/UserContext';
 
 export default function Transactions() {
+    const [transactions, setTransactions] = useState();
     const navigate = useNavigate();
     const {user} = useContext(UserContext);
 
@@ -16,15 +18,16 @@ export default function Transactions() {
                 const transactions = await axios.get('http://localhost:5000/transactions', {
                     headers: {Authorization: `Bearer ${user.token}`}
                 });
-                console.log(transactions);
+                setTransactions(transactions.data);
             } catch(e) {
                 alert('Erro ao obter transações');
                 console.log(e);
             }
         })();
     }, []);
+    console.log(transactions);
 
-    return (
+    return transactions ? (
         <Container>
             <Nav>
                 <H1>Olá, {user.name}</H1>
@@ -32,7 +35,31 @@ export default function Transactions() {
             </Nav>
             
             <Registers>
-                <Div><H2>Não há registros de entrada ou saída</H2></Div>
+                   {transactions.length > 0 ? (
+                        <Values>
+                            {transactions.map(transaction => {
+                                const {date, description, value, type} = transaction;
+                                const number = parseFloat(value).toFixed(2).replace('.', ',');
+                                console.log(type);
+
+                                return (
+                                    <List>
+                                        <Group>
+                                            <Info1>{date}</Info1>
+                                            <Info2>{description}</Info2>
+                                        </Group>
+                                        <Value color={type === 'input' ? '#03AC00' : '#C70000'}>{number}</Value>
+                                    </List>
+                                )
+                            })}
+                            <Saldo>
+                                SALDO
+                                <Value color={'#03AC00'}>{parseFloat(15).toFixed(2).replace('.', ',')}</Value> 
+                            </Saldo>
+                        </Values>
+                   ) : (
+                       <Div><H2>Não há registros de entrada ou saída</H2></Div>
+                   )}
             </Registers>
 
             <Footer>
@@ -46,7 +73,7 @@ export default function Transactions() {
                 </Transaction>
             </Footer>
         </Container>
-    );
+    ) : <p>Carregando</p>;
 }
 
 const Container = styled.div`
@@ -77,10 +104,12 @@ const Exit = styled.div`
 `;
 
 const Registers = styled.div`
-    min-width: 326px;
+    left: 25px;
+    right: 25px;
     height: calc(100vh - 230px);
     margin: 15px 0;
     border-radius: 5px;
+    position: absolute;
     background: #FFFFFF;
 `;
 
@@ -146,4 +175,51 @@ const P = styled.div`
     line-height: 20px;
     color: #FFFFFF;
     font-family: 'Raleway';
+`;
+
+const Values = styled.div`
+    width: 100%;
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+    font-family: 'Raleway';
+`;
+
+const List = styled.div`
+    font-size: 18px;
+    line-height: 19px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Group = styled.div`
+    margin: 10px;
+    display: flex;
+`;
+
+const Info1 = styled.p`
+    margin-right: 10px;
+    color: #C6C6C6;
+`;
+
+const Info2 = styled.p`
+    color: #000000;
+`;
+
+const Value = styled.p`
+    font-weight: initial;
+    color: ${props => props.color};
+`;
+
+const Saldo = styled.h1`
+    width: calc(100% - 35px);
+    bottom: 20px;
+    font-size: 17px;
+    font-weight: 700;
+    line-height: 20px;
+    margin-left: 10px;
+    display: flex;
+    position: absolute;
+    justify-content: space-between;
 `;
